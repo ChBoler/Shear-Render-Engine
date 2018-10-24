ee9553f8bf68db6659ac8cfab22e0c96ca98d46d
ChBoler <chboler@hotmail.com>
2018-10-24 16:30:26 -0400

    2DPolygon Prototyping
    Way too much sloppy vector math, and lots of variables that need renamed

--- Base 2D Node.gd
+++ Base 2D Node.gd
@@ -7,7 +7,10 @@ extends Node2D
 
 # TODO - Stress test grabbing width from settings
 var screenWidth = ProjectSettings.get_setting("display/window/size/width")
-var vanishingPoint = Vector2(screenWidth / 2, 100)
+var screenHeight = ProjectSettings.get_setting("display/window/size/height")
+var vanishX = screenWidth / 2
+var vanishY = 100
+var vanishingPoint = Vector2(vanishX, vanishY)
 
 # Ugly 2D Array Declaration
 ### THIS IS NOT A GRID, BECAUSE GRIDS ARE INFLEXIBLE ###
@@ -49,7 +52,7 @@ func _draw():
 	
 	var _array = [Vector2(0, 0), Vector2 (100, 0), Vector2 (100, 100), Vector2(0, 100)]
 	var _colorarray = [Color("FFFFFF"), Color("FFFFFF"), Color("FFFFFF"), Color("FFF11F")]
-	var _uvs = [Vector2(0, 0), Vector2(637, 0), Vector2(637, 637), Vector2(0, 637)]  #no idea
+	var _uvs = [Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1)]  #no idea
 	var _texture = load("res://Textures/TestImage.PNG")
 	
 	draw_polygon(_array, _colorarray, _uvs, _texture)
@@ -57,52 +60,112 @@ func _draw():
 	#doesnt look warpable
 	#draw_texture_rect(_texture, Rect2 rect, bool tile, Color modulate=Color( 1, 1, 1, 1 ))
 	
-	# Ok now a quiz: draw a 1x1 square in the center of the screen towards the vanishing point.
-	var midPoint = (screenWidth / 2)
+	# Ok now a quiz: draw a square in the center of the screen towards the vanishing point.
+	var midWidth = (screenWidth / 2)
+	var midHeight = (screenHeight / 2)
 	var botWidth = 600
 	var sideWidth = 150
 	var sideVolume = 20
 	var green = Color(0, 255, 0)
 	#var topWidth = 400
 	
+	# already defined dummy
+	#var vanishingPoint = Vector2(screenWidth / 2, )
+	
+	var worldPosition = Vector2(0, 0)
+	
+	var blockWidth = 100
+	var blockHeight = 20
+	var blockDepth = 75
+	
+	# =============== Front Face ===============
+	
+	# TODO - combine top left and right?
+	
+	var topRightX = midWidth + (blockWidth / 2)
+	var topRightY = midHeight - (blockHeight / 2)
+	var topRightPoint = Vector2(topRightX, topRightY)
+	
+	var topLeftX = midWidth - (blockWidth / 2)
+	var topLeftY = midHeight - (blockHeight / 2)
+	var topLeftPoint = Vector2(topLeftX, topLeftY)
+	
+	var botRightPoint = Vector2(midWidth + (blockWidth / 2), midHeight + (blockHeight / 2))
+	var botLeftPoint = Vector2(midWidth - (blockWidth / 2), midHeight + (blockHeight / 2)))
+	
+	var fpointArray = [topLeftPoint, topRightPoint, botRightPoint, botLeftPoint]
+	
+	# THING GETS DRAWN HERE
+	draw_polygon(fpointArray, _colorarray, _uvs, _texture)
+	
+	# ====== Math Setting Stage ======
+	# x3 = (((y1 − y3) / slope) - x1) * -1   where...
+	# (x1, y1) is the origin point (x2, y2) is the vanishing point and (x3, y3) is the middle-ish point
+	
+	# Use same slope for top and bot lines to push the 2.5D narrative. Maybe. Play with this!
+	var leftSlope = (vanishY - topLeftY) / (vanishX - topLeftX)
+	var rightSlope = (vanishY - topRightY) / (vanishX - topRightX)
+	
+	# =============== Top Face ===============
+	
+	# TODO - put this in a method to find the damn point
+	var leftMathPoint = Vector2((((topLeftY − vanishY) / leftSlope) - topLeftX) * -1, topLeftY - blockDepth)
+	var rightMathPoint = Vector2((((topRightY − vanishY) / rightSlope) - topRightX) * -1, topRightY - blockDepth)
+	
+	var tpointArray = [topLeftPoint, leftMathPoint, rightMathPoint, topRightPoint]
+	
+	draw_polygon(tpointArray, _colorarray, _uvs, _texture)
+	
+	# =============== Left Face ===============
+	# Check if face even shows
+	if(true){
+		var placeh = "aasda"
+	}
+	
+	# =============== Right Face ===============
+	# Check if face even shows
+	if(true){
+		var placehoo = "ddasd"
+	}
+	
 	# =============== Bottom Wall ===============
 	# 	Bottom of wall
-	draw_line(Vector2(midPoint - (botWidth / 2), 500), 
-	Vector2(midPoint + (botWidth / 2), 500), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2), 500), 
+	#Vector2(midPoint + (botWidth / 2), 500), green, 1)
 	
 	# 	Top of wall
-	draw_line(Vector2(midPoint - (botWidth / 2), 475), 
-	Vector2(midPoint + (botWidth / 2), 475), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2), 475), 
+	#Vector2(midPoint + (botWidth / 2), 475), green, 1)
 	
 	#	Inner wall ridge
-	draw_line(Vector2(midPoint - (botWidth / 2), 465), 
-	Vector2(midPoint + (botWidth / 2), 465), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2), 465), 
+	#Vector2(midPoint + (botWidth / 2), 465), green, 1)
 	
 	#	Right of wall
-	draw_line(Vector2(midPoint + (botWidth / 2), 475),
-	Vector2(midPoint + (botWidth / 2), 500), green, 1)
+	#draw_line(Vector2(midPoint + (botWidth / 2), 475),
+	#Vector2(midPoint + (botWidth / 2), 500), green, 1)
 	
 	#	Left of wall
-	draw_line(Vector2(midPoint - (botWidth / 2), 475),
-	Vector2(midPoint - (botWidth / 2), 500), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2), 475),
+	#Vector2(midPoint - (botWidth / 2), 500), green, 1)
 	
 	# =============== Left Wall ===============
 	#	Outer ridge
-	draw_line(Vector2(midPoint - (botWidth / 2), 475), 
-	Vector2(midPoint - (botWidth / 2), 475 - sideWidth), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2), 475), 
+	#Vector2(midPoint - (botWidth / 2), 475 - sideWidth), green, 1)
 	
 	#	Inner ridge
-	draw_line(Vector2(midPoint - (botWidth / 2) + sideVolume, 475), 
-	Vector2(midPoint - (botWidth / 2) + sideVolume, 475 - sideWidth), green, 1)
+	#draw_line(Vector2(midPoint - (botWidth / 2) + sideVolume, 475), 
+	#Vector2(midPoint - (botWidth / 2) + sideVolume, 475 - sideWidth), green, 1)
 	
 	# =============== Right Wall ===============
 	#	Outer ridge
-	draw_line(Vector2(midPoint + (botWidth / 2), 475), 
-	Vector2(midPoint + (botWidth / 2), 475 - sideWidth), green, 1)
+	#draw_line(Vector2(midPoint + (botWidth / 2), 475), 
+	#Vector2(midPoint + (botWidth / 2), 475 - sideWidth), green, 1)
 	
 	#	Inner ridge
-	draw_line(Vector2(midPoint + (botWidth / 2) - sideVolume, 475), 
-	Vector2(midPoint + (botWidth / 2) - sideVolume, 475 - sideWidth), green, 1)
+	#draw_line(Vector2(midPoint + (botWidth / 2) - sideVolume, 475), 
+	#Vector2(midPoint + (botWidth / 2) - sideVolume, 475 - sideWidth), green, 1)
 	
 	# =============== Top Wall ===============
 
